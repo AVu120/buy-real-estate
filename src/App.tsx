25a3df96b6014b8a87e13a75f7de537d84267c5a
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-08-01 14:32:42 +1000

    Dynamically render mock backend data onto UI (onto property cards in Results & 'Saved Properties' columns).

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import styles from "./App.module.css";
 import { testState } from "../src/utils/testState";
 import PropertyCard from "../src/components/property-card/PropertyCard";
+import { useState } from "react";
 
 function App() {
+  const [state, setState] = useState(testState);
+
   return (
     <div className={styles.App}>
       <div className={styles.columns}>
         <div className={styles.column}>
           <h2 className={styles.heading}>Results</h2>
           <div className={styles.results}>
-            <PropertyCard />
-            <PropertyCard />
-            <PropertyCard />
+            {state?.results.map((result) => (
+              <PropertyCard
+                price={result.price}
+                agencyColor={result.agency.brandingColors.primary}
+                agencyLogoUrl={result.agency.logo}
+                mainImageUrl={result.mainImage}
+              />
+            ))}
           </div>
         </div>
         <div className={styles.column}>
           <h2 className={styles.heading}>Saved Properties</h2>
           <div className={styles.saved_properties}>
-            <PropertyCard />
-            <PropertyCard />
+            {state?.saved.map((savedResult) => (
+              <PropertyCard
+                price={savedResult.price}
+                agencyColor={savedResult.agency.brandingColors.primary}
+                agencyLogoUrl={savedResult.agency.logo}
+                mainImageUrl={savedResult.mainImage}
+              />
+            ))}
           </div>
         </div>
       </div>
--- src/components/property-card/PropertyCard.tsx
+++ src/components/property-card/PropertyCard.tsx
@@ -1,54 +1,39 @@
 import styles from "./PropertyCard.module.css";
 
-const testData = {
-  price: "$726,500",
-  agency: {
-    brandingColors: {
-      primary: "#ffe512",
-    },
-    logo: "https://i1.au.reastatic.net/170x32/d9e65c666e427e655fb63dcfe73f50d4ac7ff9a58c173db9474bd92e75b01070/main.gif",
-  },
-  id: "1",
-  mainImage:
-    "https://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg",
-};
-
 interface IPropertyCard {
   price: string;
   agencyColor: string;
   agencyLogoUrl: string;
   mainImageUrl: string;
 }
-const PropertyCard = () =>
-  //   {
-  //   price,
-  //   agencyColor,
-  //   agencyLogoUrl,
-  //   mainImageUrl,
-  // }: IPropertyCard
-  {
-    return (
-      <div className={styles.card}>
-        <header
-          className={styles.header}
-          style={{ backgroundColor: testData.agency.brandingColors.primary }}
-        >
-          <img
-            src={testData.agency.logo}
-            alt="First National Real Estate Logo"
-            className={styles.logo}
-          />
-        </header>
+const PropertyCard = ({
+  price,
+  agencyColor,
+  agencyLogoUrl,
+  mainImageUrl,
+}: IPropertyCard) => {
+  return (
+    <div className={styles.card}>
+      <header
+        className={styles.header}
+        style={{ backgroundColor: agencyColor }}
+      >
         <img
-          src={testData.mainImage}
-          alt="house for sale"
-          className={styles.house_photo}
+          src={agencyLogoUrl}
+          alt="First National Real Estate Logo"
+          className={styles.logo}
         />
-        <footer className={styles.footer}>
-          <div className={styles.price}>{testData.price}</div>
-        </footer>
-      </div>
-    );
-  };
+      </header>
+      <img
+        src={mainImageUrl}
+        alt="house for sale"
+        className={styles.house_photo}
+      />
+      <footer className={styles.footer}>
+        <div className={styles.price}>{price}</div>
+      </footer>
+    </div>
+  );
+};
 
 export default PropertyCard;
