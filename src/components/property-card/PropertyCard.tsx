e5ed27bcc11f1b3dbf82da2dfd6bf8c74c966fa6
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-08-01 16:44:48 +1000

    Merge button component into property card component for simplier code management since they're tightly coupled.

--- src/components/property-card/PropertyCard.tsx
+++ src/components/property-card/PropertyCard.tsx
@@ -31,8 +31,18 @@ const PropertyCard = ({
         alt="house for sale"
         className={styles.house_photo}
       />
-      <Button action={buttonActionType} onClick={() => buttonAction(id)} />
 
+      <button
+        type="button"
+        className={
+          buttonActionType === "add"
+            ? styles.add_property_button
+            : styles.remove_property_button
+        }
+        onClick={() => buttonAction(id)}
+      >
+        {buttonActionType === "add" ? "Add property" : "Remove property"}
+      </button>
       <footer className={styles.footer}>
         <div className={styles.price}>{price}</div>
       </footer>
