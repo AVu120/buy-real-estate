6f6e0a931ea99b82e4dee822e47318b721bbaf58
Anthony Hien Vu <anthony.vu@team.telstra.com>
2021-08-15 08:50:48 +1000

    Update test state/data.

--- src/utils/testState.ts
+++ src/utils/testState.ts
@@ -1,7 +1,7 @@
 export const testState = {
   results: [
     {
-      price: "$726,500",
+      price: "$743,500",
       agency: {
         brandingColors: {
           primary: "#ffe512",
@@ -10,10 +10,10 @@ export const testState = {
       },
       id: "1",
       mainImage:
-        "https://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg",
+        "https://i2.au.reastatic.net/800x600-format=webp/b6a317b930cb0c3139aeece28b5dc463a8bf02ca1a94ab1923f436a5daeacdb2/image.jpg",
     },
     {
-      price: "$560,520",
+      price: "$660,520",
       agency: {
         brandingColors: {
           primary: "#fcfa3b",
@@ -22,10 +22,10 @@ export const testState = {
       },
       id: "2",
       mainImage:
-        "https://i1.au.reastatic.net/640x480/88586227f9176f602d5c19cf06261108dbb29f03e30d1c4ce9fc2b51fb1e4bd6/main.jpg",
+        "https://i2.au.reastatic.net/800x600-format=webp/5e4c7ada87e08e48f9982cfba5988a82faed8499bb101eea695be6c18c74a942/image.jpg",
     },
     {
-      price: "$826,500",
+      price: "$830,500",
       agency: {
         brandingColors: {
           primary: "#57B5E0",
@@ -34,22 +34,58 @@ export const testState = {
       },
       id: "3",
       mainImage:
-        "https://i4.au.reastatic.net/640x480/98cee1b2a3a64329921fc38f7e2926a78d41fcc683fc48fb8a8ef2999b14c027/main.jpg",
+        "https://i2.au.reastatic.net/800x600-format=webp/3f944265cd25c555d18a6d2f8b731aad2e3ca1f6da8adc74cff3e37aae94cb52/image.jpg",
     },
-  ],
-
-  saved: [
     {
-      price: "$526,500",
+      price: "$800,500",
+      agency: {
+        brandingColors: {
+          primary: "#ffe512",
+        },
+        logo: "https://i1.au.reastatic.net/170x32/d9e65c666e427e655fb63dcfe73f50d4ac7ff9a58c173db9474bd92e75b01070/main.gif",
+      },
+      id: "4",
+      mainImage:
+        "https://i2.au.reastatic.net/1000x750-format=webp/b00cb741fce13f4c723e2769f23c8befb0f0962194aceefbbedf03dd31631106/image.jpg",
+    },
+    {
+      price: "$1,100,500",
+      agency: {
+        brandingColors: {
+          primary: "#ffe512",
+        },
+        logo: "https://i4.au.reastatic.net/170x32/a3bd69c1a5c651eca02dd829fdd4d11d1a0b3bb6b00db79410817d17067bd495/main.gif",
+      },
+      id: "5",
+      mainImage:
+        "https://i2.au.reastatic.net/1000x750-format=webp/c6785376fb626fb4aaa31819005106a782f887b0c1fd669743bc6195505ab9ef/image.jpg",
+    },
+    {
+      price: "$850,500",
       agency: {
         brandingColors: {
           primary: "#000000",
         },
         logo: "https://i2.au.reastatic.net/170x32/3015ba9710c7e3ddc2ac30f45fd7906df5b04e442a7f6948f75a6029b8b871e2/main.gif",
       },
-      id: "4",
+      id: "6",
       mainImage:
-        "https://i2.au.reastatic.net/640x480/5e84d96722dda3ea2a084d6935677f64872d1d760562d530c3cabfcb7bcda9c2/main.jpg",
+        "https://i2.au.reastatic.net/800x600-format=webp/b4a1ce527498d7f8f6f77205e0816879f93157b4c6b81e5f4ac2c5a9e05c2084/image.jpg",
+    },
+  ],
+
+  saved: [
+    {
+      price: "$743,500",
+      agency: {
+        brandingColors: {
+          primary: "#ffe512",
+        },
+        logo: "https://i1.au.reastatic.net/170x32/d9e65c666e427e655fb63dcfe73f50d4ac7ff9a58c173db9474bd92e75b01070/main.gif",
+      },
+      id: "1",
+      mainImage:
+        "https://i2.au.reastatic.net/800x600-format=webp/b6a317b930cb0c3139aeece28b5dc463a8bf02ca1a94ab1923f436a5daeacdb2/image.jpg",
     },
   ],
 };
